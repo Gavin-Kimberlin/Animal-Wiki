c0dbbe8d96b216042ccdd25f1697e43440f2d69d
dominic montelongo and Gavin Kimberlin <gavin@>
2019-06-06 13:04:18 -0700

    create a website that will change to different animals based on what animal is selected.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,19 @@
-$(document).ready(function(){
-var animal = $("#animal").val();
-  if (animal === 1){
-    $('.apesDescription').show();
-    // $('.insectDescription, .jellyfishDescription, .turtleDescription').hide();
-  }  else if (animal === 2) {
-    $('insectDescription').show();
-    $('apesDescription, jellyfishDescription, turtleDescription').hide();
-  }  else if (animal === 3) {
-    $('jellyfishDescription').show();
-    $('apesDescription, insectfishDescription, turtleDescription').hide();
-  }  else (animal === 4) {
-    $('turtleDescription').show();
-    $('apesDescription, insectDescription, jellyfishDescription').hide();
-  }
-})
+$(document).ready(function(event){
+  $(".form-control#animal").click(function() {
+    var animal = parseInt($("#animal").val());
+    if (animal === 1){
+      $('.apesDescription').show();
+      $('.insectDescription, .jellyfishDescription, .turtleDescription').hide();
+    } else if (animal === 2) {
+      $('.insectDescription').show();
+      $('.apesDescription, .jellyfishDescription, .turtleDescription').hide();
+    } else if (animal === 3) {
+      $('.jellyfishDescription').show();
+      $('.apesDescription, .insectDescription, .turtleDescription').hide();
+    } else if (animal ===4){
+      $('.turtleDescription').show();
+      $('.apesDescription, .insectDescription, .jellyfishDescription').hide();
+    }
+    console.log(animal);
+  });
+});
