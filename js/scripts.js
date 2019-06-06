314dbd8e4c929ab62129c8d08ecdcdece95196a8
dominic montelongo and Gavin Kimberlin <dominic@>
2019-06-06 10:46:00 -0700

    Add html, css, and js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-
+var animal = $("#animal").val();
+  if (animal === 1){
+    $('.apesDescription').show();
+    // $('.insectDescription, .jellyfishDescription, .turtleDescription').hide();
+  }  else if (animal === 2) {
+    $('insectDescription').show();
+    $('apesDescription, jellyfishDescription, turtleDescription').hide();
+  }  else if (animal === 3) {
+    $('jellyfishDescription').show();
+    $('apesDescription, insectfishDescription, turtleDescription').hide();
+  }  else (animal === 4) {
+    $('turtleDescription').show();
+    $('apesDescription, insectDescription, jellyfishDescription').hide();
+  }
 })
